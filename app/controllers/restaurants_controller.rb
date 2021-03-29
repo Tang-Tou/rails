fd43fc951d9dd9fd98d1a27a1a6574430fad141f
Tang Tou <tyu4520@gmail.com>
2021-03-29 16:35:38 +0800

    新增餐廳新增表

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,8 @@
 class RestaurantsController < ApplicationController
   def index
   end
+
+  def new
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # get '/restaurants', { to: 'a#b'}
-  get '/hello', { to: 'a#b'}
+  # get '/hello', { to: 'a#b'}
   # 多個對同一個hash也可以
-  get '/', to: 'a#b'
+  # get '/', to: 'a#b'
   get '/restaurants', { to: 'restaurants#index'}
+  get '/new_restaurants', { to: 'restaurants#new'}
 end
