4430cc8acdd27a7aac3eeb776ef05c0d08b47e84
Tang Tou <tyu4520@gmail.com>
2021-03-30 00:02:34 +0800

    修改筆記部份

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,8 +1,21 @@
 class RestaurantsController < ApplicationController
+  # action
   def index
+    @aa = 'hi'
   end
-
+  # rails自動會去views找對應的檔案
+  # action不做事情 = 沒有執行任何程式
   def new
   end
   
+  def list
+    # 寫入資料庫
+    # redirect -> 列表頁
+    # render html: "title is #{params[:title]}"
+    # redirect_to '/restaurants'
+    # p restaurants_path
+    redirect_to restaurants_path
+    # path是個方法
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   # get '/restaurants', { to: 'a#b'}
   # get '/hello', { to: 'a#b'}
-  # 多個對同一個hash也可以
+  # 多個對同一個hash ( controller名  action 名 ) 也可以
   # get '/', to: 'a#b'
   get '/restaurants', { to: 'restaurants#index'}
   get '/new_restaurants', { to: 'restaurants#new'}
+  # post '/restaurant_list', to: 'restaurants#list'
+  post :restaurant_list, to: 'restaurants#list', path: '/ccc'
+  # 得到路徑 指向 叫做rs的c裡面ㄉ叫做index的action 然後
+  # path這個方法代表的是他指向的路徑
 end
